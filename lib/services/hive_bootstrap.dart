6efaeb46b24b1d942324161230ba08f7bb98dd67
Ruslan Sharmanov <dobro5691@gmail.com>
2026-02-22 03:21:05 +0100

    IRIDA: persist AI request journal (Hive) for diagnostics

--- lib/services/hive_bootstrap.dart
+++ lib/services/hive_bootstrap.dart
@@ -1,6 +1,7 @@
 import 'package:hive_flutter/hive_flutter.dart';
 
 import '../models/diagnosis_model.dart';
+import '../models/ai_journal_entry.dart';
 import '../models/gender.dart';
 
 class HiveBootstrap {
@@ -16,6 +17,15 @@ class HiveBootstrap {
     }
     if (!Hive.isAdapterRegistered(1)) {
       Hive.registerAdapter(DiagnosisAdapter());
+    }
+
+    if (!Hive.isAdapterRegistered(10)) {
+      Hive.registerAdapter(AiJournalEntryAdapter());
+    }
+
+    // Ensure AI journal box exists early (diagnostics / tracing).
+    if (!Hive.isBoxOpen('ai_journal')) {
+      await Hive.openBox<AiJournalEntry>('ai_journal');
     }
 
     _ready = true;
